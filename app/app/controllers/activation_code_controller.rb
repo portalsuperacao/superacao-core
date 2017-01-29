49aa2c6515150101aa51d148794bb8486c8487a1
Michel Bottan <michel.bottan@gmail.com>
2017-01-29 13:01:34 -0200

    Controller improvements on validation/error handling

--- app/app/controllers/activation_code_controller.rb
+++ app/app/controllers/activation_code_controller.rb
@@ -4,22 +4,26 @@ class ActivationCodeController < BaseController
   def activate
     activation_code = ActivationCode.find_by_code(params[:code])
 
-    if activation_code
-       unless activation_code.activated
-         participant = activation_code.participant
-         participant.uid = @current_uid
-         participant.save!
+    return head :not_found unless activation_code
+    return head :unprocessable_entity if activation_code.activated
 
-         activation_code.activated = true
-         activation_code.activated_at = Date.today
-         activation_code.save
+    participant = activation_code.participant
+    participant.transaction do
+      begin
+        participant.uid = @current_uid
+        participant.save!
 
-         render json: participant
-       else
-         render status: :unprocessable_entity, json: {failed: ['Activation code already used']}
-       end
-    else
-       render status: :not_found, json: {failed: ['Activation code not found']}
+        activation_code.activated = true
+        activation_code.activated_at = Date.today
+        activation_code.save
+
+        render json: participant
+      rescue ActiveRecord::RecordInvalid  => invalid
+        render json: invalid, status: :precondition_failed
+      rescue
+        head :internal_server_error
+      end
     end
+
   end
 end
