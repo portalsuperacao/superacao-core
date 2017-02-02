18e6b2d95baeea9cba953b4705d5a8198fbe8baf
Victor Presumido <victor.presumido@gmail.com>
2017-02-02 17:10:44 -0200

    52 search participant (#1)
    * Add pg_search gem
    
    * Add pg_search gem
    
    * Add form for search by name
    
    * Use pg_search for querying
    
    * Enable tsearch for pg_search
    
    * Keep type of participant when searching for name
    
    * Remove unused method
    
    * Remove unused route
    
    * Add full name on Participant index
    
    * Add  on top of table
    
    * Put query on input after result is returned

--- app/app/models/participant.rb
+++ app/app/models/participant.rb
@@ -12,6 +12,8 @@
 #
 
 class Participant < ApplicationRecord
+  include PgSearch
+
   alias_attribute :profile, :participant_profile
   enum pacient: [:myself, :family_member]
   enum cancer_status: [:overcome, :during_treatment]
@@ -24,14 +26,15 @@ class Participant < ApplicationRecord
   has_one :past_treatment_profile, class_name: 'TreatmentProfile', foreign_key: 'past_participant_id'
   has_many :missions
 
+  pg_search_scope :search_by_full_name,
+                  :associated_against => {
+                    :participant_profile => [:first_name, :last_name]},
+                  :using => {:tsearch => {:prefix => true}}
+
   after_create :generate_activation_code
 
   def active_trinities
      Trinity.where("#{self.type.downcase.to_sym}":  self.id, status: :active)
-  end
-
-  def profile
-    self.participant_profile
   end
 
   def name
