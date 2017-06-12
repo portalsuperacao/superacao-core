03158f4542975f76f4598d173f2e072f4a9250df
Michel Bottan <michel.bottan@gmail.com>
2017-06-12 15:28:51 -0300

    Fix auth on participant create via api

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::ParticipantsController < BaseController
   include CreateParticipant
 
-  before_action :authenticate
+  before_action :authenticate, only: :show
+  before_action only: [:create] {authenticate(true)}
 
   def trinities
     render json: @current_user.trinities, include: 'overcomer,angel,archangel'
+  end
+
+  def create
+    create_participant(true)
   end
 
   def show
